b6a66f02432cb41aa1d6eb4f2b60397b0cc3955d
Carson Graham <carson42g@gmail.com>
2019-12-02 20:00:55 -0500

    removed swap

--- src/main/kotlin/dev/mee42/day1/main.hs
+++ src/main/kotlin/dev/mee42/day1/main.hs
@@ -1,6 +1,7 @@
 fuel :: Int -> Int
 fuel x | x <= 8 = 0
-       | otherwise = fuel (x `div` 3 - 2) + x `div` 3 - 2
+       | otherwise = fuel x' + x'
+           where x' = x `div` 3 - 2
 
 answer :: IO Int
-answer = sum <$> map (fuel . read) <$> lines <$> readFile "res/day1.txt"
+answer = sum <$> map (fuel . read) <$> lines <$> readFile "res/day1.txt"c
