311648ba9c72c9ef94547fc3bbdfb2a6ff6b55ff
carson <carson42g@gmail.com>
2019-12-04 11:07:05 -0500

    cleaned it up a bitn

--- src/main/kotlin/dev/mee42/day4/main.hs
+++ src/main/kotlin/dev/mee42/day4/main.hs
@@ -5,6 +5,10 @@ unzipWith :: (a -> b -> c) -> [(a,b)] -> [c]
 unzipWith _ [] = []
 unzipWith com ((a,b):xs) = (com a b) : (unzipWith com xs)
 
+
+numOut :: Int -> [Int]
+numOut i = map (\x -> read [x] :: Int) (show i)
+
 isValid :: Int -> Bool
 isValid int = do
   let nums = map (\x -> (read [x]) :: Int) (show int) -- [Int]
@@ -15,14 +19,14 @@ isValid int = do
 
 isValid2 :: Int -> Bool
 isValid2 int = do
-  let nums = map (\x -> read [x] :: Int) (show int) 
-  let isDecending = all id (unzipWith (<=) (nums `zip` tail nums)) 
+  let nums = numOut int 
+ 
   let triples = concat $ map (\(index,(a,_,_)) -> [index,index+1,index+2]) $ filter (\(_,(a,b,c)) -> a==b&&b==c) $ zip [0..] $ zip3 nums (drop 1 nums) (drop 2 nums)
   
-  let zipped = zip [0..] $ nums `zip` (drop 1 nums)
-  let pairs = filter (\(index,(first,second)) -> not (elem index triples) && first == second) zipped 
+  
+  let pairs = filter (\(index,(first,second)) -> not (elem index triples) && first == second) $ zip [0..] $ nums `zip` (drop 1 nums) 
 
-  pairs /= [] && isDecending
+  pairs /= [] && (all id (unzipWith (<=) (nums `zip` tail nums)))
 
 
 input :: [Int]
